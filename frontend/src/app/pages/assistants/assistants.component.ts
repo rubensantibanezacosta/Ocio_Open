17a34d460b95067c065a11589bb89bb58f65d71d
rubensantibanezacosta <rubensantibanezacosta902@gmail.com>
2021-12-14 15:02:49 +0000

    fixes

--- frontend/src/app/pages/assistants/assistants.component.ts
+++ frontend/src/app/pages/assistants/assistants.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AssistantsService } from 'src/app/services/assistants.service';
 import { ErrorHandlerService } from 'src/app/services/error-handler.service';
-import {Asisstant} from '../../models/assistant'
+import { Asisstant } from '../../models/assistant'
 
 @Component({
   selector: 'app-assistants',
@@ -11,42 +11,42 @@ import {Asisstant} from '../../models/assistant'
   styleUrls: ['./assistants.component.scss']
 })
 export class AssistantsComponent implements OnInit {
-  event_id:number=this.activatedRoute.snapshot.params.event_id;
-  tittle:string="Asistentes"
-  image="../../../assets/icons/assistants_icon.png";
-  miniStar="../../../assets/icons/mini-star.png";
+  event_id: number = this.activatedRoute.snapshot.params.event_id;
+  tittle: string = "Asistentes"
+  image = "../../../assets/icons/assistants_icon.png";
+  miniStar = "../../../assets/icons/mini-star.png";
 
-  ErrorMessage:string;
+  ErrorMessage: string;
 
-  assistants:Asisstant[]=[];
-  constructor(private activatedRoute: ActivatedRoute, private assistantsService:AssistantsService,  private errorHandlerService:ErrorHandlerService) { }
+  assistants: Asisstant[] = [];
+  constructor(private activatedRoute: ActivatedRoute, private assistantsService: AssistantsService, private errorHandlerService: ErrorHandlerService) { }
 
   ngOnInit(): void {
     this.getAssistantsByEvent();
   }
 
-  getAssistantsByEvent(){
-    return this.assistantsService.getAssistantsByEvent(this.event_id).subscribe((assistants)=>{
-      this.assistants=assistants;
+  getAssistantsByEvent() {
+    return this.assistantsService.getAssistantsByEvent(this.event_id).subscribe((assistants) => {
+      this.assistants = assistants;
     },
-    (error) => {
-      this.ErrorMessage=error.error;
-      this.createModal();
+      (error) => {
+        this.ErrorMessage = error.error;
+        this.createModal();
 
-    })
+      })
   }
 
-    //Error handler modals
-    @ViewChild('modal', { read: ViewContainerRef })
-    entry!: ViewContainerRef;
-    sub!: Subscription;
-  
-  
-    createModal(){
-        this.sub = this.errorHandlerService
-          .openModal(this.entry, 'ERROR', this.ErrorMessage)
-          .subscribe((v) => {
-            //your logic
-          });
-    }
+  //Error handler modals
+  @ViewChild('modal', { read: ViewContainerRef })
+  entry!: ViewContainerRef;
+  sub!: Subscription;
+
+
+  createModal() {
+    this.sub = this.errorHandlerService
+      .openModal(this.entry, 'ERROR', this.ErrorMessage)
+      .subscribe((v) => {
+        //your logic
+      });
+  }
 }
